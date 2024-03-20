da3e25f1e60c876681c2c3f02a882074ae6be238
Sreesankar-t <125552943+Sreesankar-t@users.noreply.github.com>
2024-03-20 17:23:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({ origin: 'https://wonderinn.netlify.app', credentials: true }))
 
 app.use('/', userRouters)
 app.use('/admin', adminRouter)
